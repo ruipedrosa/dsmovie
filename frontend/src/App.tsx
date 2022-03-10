f5abf4a876950a9c3961f8246e1fb28ea195de56
Rui Pedrosa <rmps.76@gmail.com>
2022-03-10 23:44:02 +0000

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <h1>Página principal</h1>
+    <Navbar/>
   );
 }
 
---
+++ frontend/src/Components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GuithubIcon } from 'asserts/img/github.svg';
+import './styles.css'
+
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/ruipedrosa">
+                        <div className="dsmovie-contact-container">
+                            <GuithubIcon />
+                            <p className="dsmovie-contact-link">/devsuperior</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;   
