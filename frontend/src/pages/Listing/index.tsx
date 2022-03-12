a07ccadad1e7e1b3b165aca6aa912a4226debc46
Rui Pedrosa <rmps.76@gmail.com>
2022-03-12 20:41:29 +0000

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,20 +1,39 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // FORMA ERRADA - APENAS PARA TESTE
-    //axios.get('http://localhost:8080/movies?size=12&page=1')
-    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+
+    const [pageNumber,setPageNumber] = useState (0);
+
+
+    // FORMA CORRETA
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
         .then(response => {
             console.log(response.data);
+            const data = response.data as MoviePage;
+            setPageNumber(data.number);
         });
+    }, []);
+
+    // FORMA ERRADA - APENAS PARA TESTE
+    //axios.get('http://localhost:8080/movies?size=12&page=0')
+    //axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    //    .then(response => {
+    //        //console.log(response.data);
+    //        const data = response.data as MoviePage;
+    //        setPageNumber(data.number);
+    //    });
 
     
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
